f97055c292a47e8ba1224502a8bd138bd810dbe8
matosys <max@matosys.de>
2025-11-29 18:15:05 +0100

    refactor(utils): remove method `MultipleDataConfig.get_for_identifier` because functionality is already within `SingleDataItemCollection`

--- src/balderhub/data/lib/scenario_features/multiple_data_config.py
+++ src/balderhub/data/lib/scenario_features/multiple_data_config.py
@@ -1,6 +1,4 @@
 from .abstract_data_item_related_feature import AbstractDataItemRelatedFeature
-
-from ..utils.single_data_item import SingleDataItemTypeT
 from ..utils.single_data_item_collection import SingleDataItemCollection
 
 
@@ -15,15 +13,3 @@ class MultipleDataConfig(AbstractDataItemRelatedFeature):
         :return: returns the data item collection this config feature describes
         """
         raise NotImplementedError()
-
-    def get_for_identifier(self, identifier) -> SingleDataItemTypeT | None:
-        # TODO remove here and add in SingleDataItemCollection!
-
-        elems = [e for e in self.data_list if e.get_unique_identification() == identifier]
-        if len(elems) == 0:
-            return None
-        if len(elems) > 1:
-            raise ValueError(f'found more than one elements with identifier {identifier}')
-        return elems[0]
-
-
