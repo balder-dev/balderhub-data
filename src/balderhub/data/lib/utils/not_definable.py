fb09aab4b663713a4adeec17f5acf3f8cb7f8b91
matosys <max@matosys.de>
2025-11-29 12:16:24 +0100

    feat(utils): make not-definable singleton

--- src/balderhub/data/lib/utils/not_definable.py
+++ src/balderhub/data/lib/utils/not_definable.py
@@ -6,6 +6,15 @@ class _NOT_DEFINABLE_TYPE:
     Type for NON_DEFINABLE values. This object is assigned for all fields that are unable to define either by user or
     by data interacting features.
     """
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(_NOT_DEFINABLE_TYPE, cls).__new__(cls)
+        return cls._instance
+
+    def __repr__(self):
+        return "NON_DEFINABLE"
 
     def __eq__(self, other):
         return isinstance(other, _NOT_DEFINABLE_TYPE)
