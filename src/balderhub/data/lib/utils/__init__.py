4005bf1f608033333dfc2c6ed3d3900d9576f635
matosys <max@matosys.de>
2025-11-29 17:29:15 +0100

    feat(utils): introduce new helper object `LookupFieldString`

--- src/balderhub/data/lib/utils/__init__.py
+++ src/balderhub/data/lib/utils/__init__.py
@@ -1,5 +1,6 @@
 from .auto_feature_factory import AutoFeatureFactory
 from .not_definable import NOT_DEFINABLE
+from .lookup_field_string import LookupFieldString
 from .response_message import ResponseMessage
 from .response_message_list import ResponseMessageList
 from .single_data_item import SingleDataItem
@@ -7,6 +8,7 @@ from .single_data_item_collection import SingleDataItemCollection
 
 __all__ = [
     'NOT_DEFINABLE',
+    'LookupFieldString',
     'ResponseMessage',
     'ResponseMessageList',
     'SingleDataItem',
