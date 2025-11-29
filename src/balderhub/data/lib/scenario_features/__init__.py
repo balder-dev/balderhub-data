70f8fb27df000b5dfdc6a174795ee990f953ce03
matosys <max@matosys.de>
2025-11-29 09:57:59 +0100

    feat(scenario_feature): introduce new data-environment feature

--- src/balderhub/data/lib/scenario_features/__init__.py
+++ src/balderhub/data/lib/scenario_features/__init__.py
@@ -1,6 +1,7 @@
 from .abstract_data_item_related_feature import AbstractDataItemRelatedFeature
 from .accessible_multiple_data_config import AccessibleMultipleDataConfig
 from .all_multiple_data_config import AllMultipleDataConfig
+from .data_environment_feature import DataEnvironmentFeature
 from .example_field_value_provider_feature import ExampleFieldValueProviderFeature
 from .example_data_provider_feature import ExampleDataProviderFeature
 from .multiple_data_config import MultipleDataConfig
