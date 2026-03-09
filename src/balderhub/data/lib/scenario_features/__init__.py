2b8f9217cd66711c43e8eef08e9e2b4e86ed3e0b
matosys <max@matosys.de>
2026-03-09 15:39:42 +0100

    fix(scenario_features): remove example provider feature because they are moved to balderhub-crud

--- src/balderhub/data/lib/scenario_features/__init__.py
+++ src/balderhub/data/lib/scenario_features/__init__.py
@@ -3,15 +3,12 @@ from . import factories
 from .abstract_data_item_related_feature import AbstractDataItemRelatedFeature
 from .accessible_initial_data_config import AccessibleInitialDataConfig
 from .data_environment_feature import DataEnvironmentFeature
-from .example_field_value_provider_feature import ExampleFieldValueProviderFeature
-from .example_data_provider_feature import ExampleDataProviderFeature
 from .initial_data_config import InitialDataConfig
 
 
 __all__ = [
     'AbstractDataItemRelatedFeature',
     'AccessibleInitialDataConfig',
-    'ExampleFieldValueProviderFeature',
-    'ExampleDataProviderFeature',
+    'DataEnvironmentFeature',
     'InitialDataConfig',
 ]
